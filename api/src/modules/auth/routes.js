9ad52bb9f80ca6368a1e9fcf9f93629fc7d2c0a3
Serhii Chernikov <55809302+perseverance50k@users.noreply.github.com>
2024-04-20 20:21:02 +0300

    Implement basic logic for /auth/login route

--- api/src/modules/auth/routes.js
+++ api/src/modules/auth/routes.js
@@ -1,16 +1,39 @@
 const express = require("express");
 
-const { getDb } = require("../database");
+const { getCollection } = require("../database");
 
 const router = express.Router();
 
+const USERS_COLLECTION = "users";
+
 router.post("/register", async (req, res) => {
-  const db = getDb();
-  const collection = db.collection("users");
   const user = req.body;
+  const collection = getCollection(USERS_COLLECTION);
 
   const result = await collection.insertOne(user);
   res.send(result).status(201);
+});
+
+router.post("/login", async (req, res) => {
+  const credentials = req.body;
+  const collection = getCollection(USERS_COLLECTION);
+
+  const { email, password } = credentials;
+  const user = await collection.findOne({ email });
+
+  if (!user || user.password !== password) {
+    const payload = {
+      error: "Wrong credentials provided!",
+    };
+    res.send(payload).status(403);
+    return;
+  }
+
+  const payload = {
+    success: "Successfully logged in!",
+  };
+
+  res.send(payload).status(200);
 });
 
 module.exports = router;
--- api/src/modules/database/utils/index.js
+++ api/src/modules/database/utils/index.js
@@ -1,1 +1,1 @@
-module.exports = { ...require("./connectDb") };
+module.exports = { ...require("./connectDb"), ...require("./getCollection") };
