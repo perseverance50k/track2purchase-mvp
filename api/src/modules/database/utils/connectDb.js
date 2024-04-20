d8d59cd13a0fb531f5ae8e2c34a96fae31e1ea11
Serhii Chernikov <55809302+perseverance50k@users.noreply.github.com>
2024-04-20 19:26:27 +0300

    Refactor connectDb function to return singleton DB instance & make db instance global

--- api/src/modules/database/utils/connectDb.js
+++ api/src/modules/database/utils/connectDb.js
@@ -3,17 +3,17 @@ const { MongoClient } = require("mongodb");
 const connectionString = process.env.ATLAS_URI || "";
 const client = new MongoClient(connectionString);
 
+let db;
+
 const connectDb = async () => {
-  let db;
-
-  try {
-    const connection = await client.connect();
-    db = connection.db(process.env.ATLAS_DB);
-  } catch (e) {
-    console.error(e);
+  if (!db) {
+    try {
+      const connection = await client.connect();
+      db = connection.db(process.env.ATLAS_DB);
+    } catch (e) {
+      console.error(e);
+    }
   }
-
-  return db;
 };
 
-module.exports = { connectDb };
+module.exports = { db, connectDb };
