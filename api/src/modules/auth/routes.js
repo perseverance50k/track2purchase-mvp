e83783b3da5431d4c12f89aec2cd99a72bab2652
Serhii Chernikov <55809302+perseverance50k@users.noreply.github.com>
2024-04-25 09:26:33 +0300

    Enhance validation logic for user resitration/login using Joi schema

--- api/src/modules/auth/routes.js
+++ api/src/modules/auth/routes.js
@@ -1,27 +1,39 @@
 const express = require("express");
 
 const { getCollection } = require("../database");
+const { userSchema } = require("../user");
 
 const router = express.Router();
 
 const USERS_COLLECTION = "users";
 
 router.post("/register", async (req, res) => {
-  const newUser = req.body;
+  const credentials = req.body;
   const collection = getCollection(USERS_COLLECTION);
 
-  const user = await collection.findOne({ email: newUser.email });
+  const validationResult = userSchema.validate(credentials);
 
-  if (user) {
+  if (validationResult.error) {
     const payload = {
-      error: "Such a user already exist.",
+      error: "Incorrect credentials provided!",
+    };
+
+    res.send(payload).status(400);
+    return;
+  }
+
+  const isUserExist = await collection.findOne({ email: credentials.email });
+
+  if (isUserExist) {
+    const payload = {
+      error: "Such a user already exist!",
     };
 
     res.send(payload).status(403);
     return;
   }
 
-  const result = await collection.insertOne(newUser);
+  const result = await collection.insertOne(credentials);
   res.send(result).status(201);
 });
 
@@ -29,12 +41,23 @@ router.post("/login", async (req, res) => {
   const credentials = req.body;
   const collection = getCollection(USERS_COLLECTION);
 
+  const validationResult = userSchema.validate(credentials);
+
+  if (validationResult.error) {
+    const payload = {
+      error: "Incorrect credentials provided!",
+    };
+
+    res.send(payload).status(400);
+    return;
+  }
+
   const { email, password } = credentials;
   const user = await collection.findOne({ email });
 
   if (!user || user.password !== password) {
     const payload = {
-      error: "Wrong credentials provided!",
+      error: "Incorrect credentials provided!",
     };
 
     res.send(payload).status(403);
