830975e8b569e431540b73c21c33b6157fa8d998
Serhii Chernikov <55809302+perseverance50k@users.noreply.github.com>
2024-05-04 11:34:24 +0300

    Implement logic for creating authentication JWT on user login

--- api/src/modules/auth/routes.js
+++ api/src/modules/auth/routes.js
@@ -2,16 +2,19 @@ const express = require("express");
 const httpStatus = require("http-status");
 
 const { add: addUser, verify: verifyUser } = require("./service");
+const { createJwt } = require("./utils");
 
 const router = express.Router();
+
+const JWT_EXPIRATION_PERIOD = process.env.JWT_EXPIRATION_PERIOD;
 
 // --- REGISTRATION ---
 router.post("/register", async (req, res, next) => {
   const credentials = req.body;
 
   try {
-    await addUser(credentials);
-    res.send({ success: true }).status(httpStatus.CREATED);
+    const addedUser = await addUser(credentials);
+    res.send({ created: addedUser }).status(httpStatus.CREATED);
   } catch (e) {
     next(e);
   }
@@ -20,10 +23,20 @@ router.post("/register", async (req, res, next) => {
 // --- LOGIN ---
 router.post("/login", async (req, res, next) => {
   const credentials = req.body;
-  const cookies = req.cookies;
-
   try {
     const verified = await verifyUser(credentials);
+
+    const tokenPayload = {
+      sub: verified.email,
+    };
+
+    const tokenOptions = {
+      expiresIn: JWT_EXPIRATION_PERIOD,
+    };
+
+    const token = createJwt(tokenPayload, tokenOptions);
+
+    res.cookie("Authentication", token, { httpOnly: true });
     res.send(verified).status(httpStatus.OK);
   } catch (e) {
     next(e);
